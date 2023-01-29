6ec9a1838c46e7e2cd5b75fd26b216dbacea7f44
Snaccident <111965097+TamaraSovcikova@users.noreply.github.com>
2023-01-29 10:21:54 +0000

    Add files via upload

---
+++ Trimorphic number/Trimorphic number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trimorphic_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num < 100000; num++)
+            {
+                int cubed = num * num * num;
+                string scubed = cubed.ToString();
+                string z = num.ToString();
+
+                if (scubed.EndsWith(z))
+                {
+                    Console.WriteLine(num);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
