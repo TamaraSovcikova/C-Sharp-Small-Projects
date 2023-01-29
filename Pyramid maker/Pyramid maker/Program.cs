a0b773ae2d8d3042d369cd925defc93d6eb468eb
Snaccident <111965097+TamaraSovcikova@users.noreply.github.com>
2023-01-29 10:29:47 +0000

    Add files via upload

---
+++ Pyramid maker/Pyramid maker/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pyramid_maker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string end = "";
+            do
+            {
+                Console.WriteLine("Enter base size of pyramid => ");
+                int baseSize = int.Parse(Console.ReadLine());
+                if (baseSize % 2 == 1)
+                {
+                    MakeRightSideUpPyramid(baseSize);
+                    MakeUpsideDownPyramid(baseSize);
+                    Console.WriteLine("Press y to continue or any other key to exit");
+                    end = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid - Number must be an odd integer");
+                }
+            }
+            while (end == "y");
+            Console.WriteLine("Bye bye then!");
+            Console.ReadKey();
+        }        
+        static void MakeRightSideUpPyramid(int baseSize)
+        {
+            int pad = (baseSize +1) / 2;
+            int layer = 0;
+            string space = " ";
+            Console.WriteLine("\n <3 Here is your right side up pyramid <3 \n");
+            while (layer != baseSize && pad != 0)
+            {
+                Console.Write(space.PadLeft(pad));
+                for (int y = 0; y <= layer; y++)
+                {
+                    Console.Write("*");
+                }
+                layer = layer +2;
+                Console.WriteLine();
+                pad--;
+            }
+        }
+        static void MakeUpsideDownPyramid(int baseSize)
+        {
+            int pad = 1;
+            string space = " ";
+            Console.WriteLine("\n <3 Here is your upside down pyramid <3 \n");
+            while (baseSize >= 1)
+            {
+                Console.Write(space.PadLeft(pad));
+                for (int y = 0; y < baseSize; y++)
+                {
+                    Console.Write("*");
+                }
+                baseSize = baseSize - 2;                
+                Console.WriteLine();                
+                pad++;
+            }            
+        }
+    }
+}
---
+++ Roman To Decimal/Roman To Decimal/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_To_Decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string roman = "IMVVXXII";
+            int total = RomanToInt(roman);
+            Console.WriteLine(total);
+
+            Console.ReadKey();
+        }
+        static int RomanToInt(string s)
+        {
+            char[] romanNumerals = s.ToCharArray();
+            int total = 0;
+            int value = 0;
+            int followingValue = 0;
+
+            for (int i = 0; i < romanNumerals.GetLength(0); i++)
+            {
+                value = ValueInDecimal(romanNumerals[i]);
+                if (i < romanNumerals.GetLength(0) -1)
+                followingValue = ValueInDecimal(romanNumerals[(i + 1)]);  
+                else
+                followingValue = 0;
+
+            if (value < followingValue)
+                {
+                    total += followingValue - value;
+                    i++;
+                }
+                else
+                    total += value;
+            }
+            return total;
+        }
+        static int ValueInDecimal(char num)
+        {
+            int value = 0;
+            switch (num)
+            {
+                case 'I': value = 1; break;
+                case 'V': value = 5; break;
+                case 'X': value = 10; break;
+                case 'L': value = 50; break;
+                case 'C': value = 100; break;
+                case 'D': value = 500; break;
+                case 'M': value = 1000; break;
+            }
+            return value;
+        }
+    }
+}
